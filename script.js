145ed5fc4f80b89d8ffebfdef41807d2c71e7773
itasuke <itasuke@gmail.com>
2022-03-24 22:01:57 +0900

    add test pages

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const para = '<p style="color: red;">🗸 JavaScript</p>';
+    document.body.insertAdjacentHTML('beforeend', para);
+});
